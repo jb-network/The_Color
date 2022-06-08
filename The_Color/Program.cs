dce05c4308c97afbdb1fdf794579045ef4971050
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-08 03:28:20 -0500

    fixed issue: "public" missing from class properties.

--- The_Color/Program.cs
+++ The_Color/Program.cs
@@ -9,18 +9,19 @@ Goals:
 */
 
 Color Constructor_Color = new Color(255, 38, 233);
-Color Static_Property_Color = Color.Yellow_Color;
-Console.WriteLine($"{Constructor_Color.red}");
-Console.WriteLine($"{Static_Property_Color}");
+Color Static_Property_Color = Color.Purple_Color;
 
-
+Console.WriteLine($"Constructor Color: \tRed = {Constructor_Color.red} \tGreen = {Constructor_Color.green} \tBlue = {Constructor_Color.blue}");
+Console.WriteLine($"Static Property Color: \tRed = {Static_Property_Color.red} \tGreen = {Static_Property_Color.green} \tBlue = {Static_Property_Color.blue}");
+Console.WriteLine("Press any key to end the program");
+Console.ReadKey();
 
 public class Color
 {
     //byte type used to ensure settings are between 0 and 255
-    byte red { get; }
-    byte green { get; }
-    byte blue { get; }
+    public byte red { get; }
+    public byte green { get; }
+    public byte blue { get; }
 
     public Color(byte Red, byte Green, byte Blue)
     {
@@ -31,14 +32,14 @@ public class Color
 
     // based on pg. 171 - Static Properties 
     //Sets Color type to name and passes RGB setting to constructor
-    public static Color White_Color { get;  } = new Color(255, 255, 255);
-    public static Color Black_Color { get; } = new Color(0, 0, 0);
-    public static Color Red_Color { get; } = new Color(255, 0, 0);
-    public static Color Orange_Color { get; } = new Color(255,165,0);
-    public static Color Yellow_Color { get; } = new Color(255,255,0);
-    public static Color Green_Color { get; } = new Color(0,128,0);
-    public static Color Blue_Color { get; } = new Color(0,0,255);
-    public static Color Purple_Color { get; } = new Color(128,0,128);
+    public static Color White_Color = new Color(255, 255, 255);
+    public static Color Black_Color = new Color(0, 0, 0);
+    public static Color Red_Color = new Color(255, 0, 0);
+    public static Color Orange_Color = new Color(255,165,0);
+    public static Color Yellow_Color = new Color(255,255,0);
+    public static Color Green_Color = new Color(0,128,0);
+    public static Color Blue_Color = new Color(0,0,255);
+    public static Color Purple_Color = new Color(128,0,128);
     
 
 }
